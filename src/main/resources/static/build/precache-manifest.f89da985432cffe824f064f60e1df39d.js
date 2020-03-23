bd1e13fe49b5677fd76debb51128c021fa647340
davidrnogueira <davidrnogueira@hotmail.com>
2020-03-22 21:53:47 -0700

    Build commit

---
+++ src/main/resources/static/build/precache-manifest.f89da985432cffe824f064f60e1df39d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a503817553b9b0a85896de1359e520c5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4c0d70ae064c78d7d3b2",
+    "url": "/static/css/main.1bfa3620.chunk.css"
+  },
+  {
+    "revision": "1b416e763d01da9abff4",
+    "url": "/static/js/2.a822842c.chunk.js"
+  },
+  {
+    "revision": "c98fee057575cee7ebcc2516a1b6c341",
+    "url": "/static/js/2.a822842c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4c0d70ae064c78d7d3b2",
+    "url": "/static/js/main.73366e66.chunk.js"
+  },
+  {
+    "revision": "ba0c647ee4a2790e8cce",
+    "url": "/static/js/runtime-main.ab679358.js"
+  },
+  {
+    "revision": "3a9b89309d5a6c047ec2330dede86ca9",
+    "url": "/static/media/NoAvatar.3a9b8930.png"
+  }
+]);
